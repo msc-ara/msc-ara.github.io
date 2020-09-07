8f1658aef26a0af0d611d9367f1b2161299e6775
Mairon Costa <maironmendes@yahoo.com.br>
2020-09-07 14:34:58 -0300

    detail is working

--- resources/js/ara/detail.js
+++ resources/js/ara/detail.js
@@ -17,7 +17,38 @@ async function loadDetail () {
         document.getElementById("idPostCodeForm").value = post.code;
         document.getElementById("idPostTitle").innerHTML = post.title;
 
+        await loadCommentsFromPost(post);
+
     }
+
+}
+
+async function loadCommentsFromPost (post) {
+
+    console.info("post.comments: " + JSON.stringify(post.comments));
+
+    let output = "";
+    for (let obj in post.comments) {
+
+        let postComment = PostComment(post.comments[obj].Date, post.comments[obj].Comment);
+        (output += `
+            <div class="row form-group">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">${postComment.comment}</p>
+                        </div>
+                        <div class="card-footer text-muted">
+                            <small class="text-muted">${postComment.date}</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `)
+
+    }
+    console.info("output: " + output);
+    document.getElementById("idPostComments").innerHTML = output;
 
 }
 
