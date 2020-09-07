c9d9b5262a5f59201876672af33903c81962f491
Mairon Costa <maironmendes@yahoo.com.br>
2020-09-07 00:35:57 -0300

    removed some comment in the code.

--- resources/js/detail.js
+++ resources/js/detail.js
@@ -6,15 +6,12 @@ async function load () {
     const code = urlParams.get("code");
     const response = await getPostByCode(code);
 
-    console.info("<<<<<<<<<response.status>>>>>>>>> " + response.status);
     if (response.status == 200) {
 
         const json = JSON.stringify(await response.json());
         const parse = JSON.parse(json);
-        // console.info("parse: " + JSON.stringify(parse));
         const post = Post(parse.Code, parse.Title, parse.ActiveDays, parse.Comments);
 
-        // console.info("<<<<<<<<<post>>>>>>>>> " + JSON.stringify(post));
         document.getElementById("idPostCode").innerHTML = post.code;
         document.getElementById("idPostCodeForm").value = post.code;
         document.getElementById("idPostTitle").innerHTML = post.title;
@@ -30,10 +27,9 @@ if (submitFormRegister != null) {
 
         let form = document.getElementById("formAddComment");
 
-        console.info("form.comment: " + form.comment.value);
+        // console.info("form.comment: " + form.comment.value);
         let response = await addComment(form.postCode.value, form.comment.value);
         const json = JSON.stringify(await response.json());
-        console.info("response.body: " + json);
 
         if (response.status == 201) {
 
