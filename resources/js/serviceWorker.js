fb9a390cbd1b2693937edf5a1d8c2a7818bac047
Mairon Costa <maironmendes@yahoo.com.br>
2020-09-09 20:37:09 -0300

    added some assets to the cache; change the cache strategy then networking first and then cache for cache first and then networking

--- resources/js/serviceWorker.js
+++ resources/js/serviceWorker.js
@@ -1,8 +1,22 @@
-const CACHE_NAME = "ara-web-v1.2"
+const CACHE_NAME = "ara-web-v1.3"
 const assets = [
     "/",
     "/index.html",
     "/resources/js/app.js",
+    "/resources/js/bootstrap4/offcanvas.js",
+    "/resources/images/icon/apple-icon-57x57.png",
+    "/resources/images/icon/apple-icon-60x60.png",
+    "/resources/images/icon/apple-icon-72x72.png",
+    "/resources/images/icon/apple-icon-76x76.png",
+    "/resources/images/icon/apple-icon-114x114.png",
+    "/resources/images/icon/apple-icon-120x120.png",
+    "/resources/images/icon/apple-icon-144x144.png",
+    "/resources/images/icon/apple-icon-152x152.png",
+    "/resources/images/icon/apple-icon-180x180.png",
+    "/resources/images/icon/android-icon-192x192.png",
+    "/resources/images/icon/favicon-32x32.png",
+    "/resources/images/icon/favicon-96x96.png",
+    "/resources/images/icon/favicon-16x16.png",
 ]
 
 self.addEventListener("install", installEvent => {
@@ -42,33 +56,33 @@ self.addEventListener('activate', (evt) => {
 // https://blog.bitsrc.io/5-service-worker-caching-strategies-for-your-next-pwa-app-58539f156f52
 
 // Cache First, then Network - Strategy
-// self.addEventListener('fetch', function (event) {
-//     event.respondWith(
-//         caches.open(cacheName)
-//             .then(function(cache) {
-//                 cache.match(event.request)
-//                     .then( function(cacheResponse) {
-//                         if(cacheResponse)
-//                             return cacheResponse
-//                         else
-//                             return fetch(event.request)
-//                                 .then(function(networkResponse) {
-//                                     cache.put(event.request, networkResponse.clone())
-//                                     return networkResponse
-//                                 })
-//                     })
-//             })
-//     )
-// });
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.open(cacheName)
+            .then(function(cache) {
+                cache.match(event.request)
+                    .then( function(cacheResponse) {
+                        if(cacheResponse)
+                            return cacheResponse
+                        else
+                            return fetch(event.request)
+                                .then(function(networkResponse) {
+                                    cache.put(event.request, networkResponse.clone())
+                                    return networkResponse
+                                })
+                    })
+            })
+    )
+});
 
 // Network First, then Cache - Strategy
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        fetch(event.request).catch(function() {
-            return caches.match(event.request);
-        })
-    )
-})
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(
+//         fetch(event.request).catch(function() {
+//             return caches.match(event.request);
+//         })
+//     )
+// })
 
 // Cache Only - Strategy
 // self.addEventListener('fetch', function (event) {
