8668131c178646bc69da10dbce27ad62d149b5f1
Mairon Costa <maironmendes@yahoo.com.br>
2020-09-09 20:25:39 -0300

    fix the randomic index for phrases.

--- resources/js/ara/index.js
+++ resources/js/ara/index.js
@@ -1,25 +1,25 @@
 
 let phrasesMap = new Map(
     [
-        [0, new Phrase("#FFFFFF", "#244c90", "I'm drinking a chai...")],
         [1, new Phrase("#CB3535", "#000000", "Flamengo will be brasileirao 2020 champion?")],
         [2, new Phrase("#358fcb", "#FFFFFF", "How about travel to Paris?")],
         [3, new Phrase("#ffffff", "#000000", "I wanna change my computer... any sugestion?")],
         [4, new Phrase("#CB3535", "#ffffff", "What to drink in Singapure?")],
         [5, new Phrase("#399024", "#ffffff", "Who will travel this summer and for where?")],
         [6, new Phrase("#244c90", "#ffffff", "Let's enjoy the night!")],
+        [7, new Phrase("#FFFFFF", "#244c90", "I'm drinking a chai...")],
     ]
 );
 
 function loadIndex() {
 
-    const howManyRandomNumber = phrasesMap.size + 1;
+    const howManyRandomNumber = phrasesMap.size;
     // console.info("howManyRandomNumber: " + howManyRandomNumber);
-    let index = Math.floor(Math.random() * howManyRandomNumber);
+    let index = Math.floor((Math.random() * howManyRandomNumber) + 1);
     // console.info("index: " + index);
 
     let phrase = phrasesMap.get(index);
-    // console.info("phrase: " + phrase);
+    // console.info("phrase: " + JSON.stringify(phrase));
 
     let body = document.getElementById("idBodyIndex");
     body.style=`background-color:${phrase.backgroundColor};color:${phrase.fontColor}`;
